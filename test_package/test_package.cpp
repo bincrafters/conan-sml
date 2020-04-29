dd74ade83b072aa53b98d3bb93f4fc966d549da0
Alexander Zaitsev <zamazan4ik@tut.by>
2020-04-30 02:20:24 +0300

    Add test_package which should work on msvc

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,46 +1,54 @@
-//
-// Copyright (c) 2016-2019 Kris Jusiak (kris at jusiak dot net)
-//
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-//
-
+// $CXX -std=c++14 hello_world.cpp
 #include <boost/sml.hpp>
-
 #include <cassert>
-#include <iostream>
 
 namespace sml = boost::sml;
 
 namespace {
-struct e1 {};
-struct e2 {};
-struct e3 {};
+struct release {};
+struct ack {};
+struct fin {};
+struct timeout {};
 
-struct states {
-  auto operator()() const noexcept {
+const auto is_ack_valid = [](const ack&) { return true; };
+const auto is_fin_valid = [](const fin&) { return true; };
+
+const auto send_fin = [] {};
+const auto send_ack = [] {};
+
+class established;
+class fin_wait_1;
+class fin_wait_2;
+class timed_wait;
+
+struct hello_world {
+  auto operator()() const {
     using namespace sml;
-    const auto idle = state<class idle>;
-    // clang-format off
     return make_transition_table(
-       *idle + event<e1> = "s1"_s
-      , "s1"_s + sml::on_entry<_> / [] { std::cout << "s1 on entry" << std::endl; }
-      , "s1"_s + sml::on_exit<_> / [] { std::cout << "s1 on exit" << std::endl; }
-      , "s1"_s + event<e2> = state<class s2>
-      , state<class s2> + event<e3> = X
+      *state<established> + event<release> / send_fin = state<fin_wait_1>,
+       state<fin_wait_1> + event<ack> [ is_ack_valid ] = state<fin_wait_2>,
+       state<fin_wait_2> + event<fin> [ is_fin_valid ] / send_ack = state<timed_wait>,
+       state<timed_wait> + event<timeout> / send_ack = X
     );
-    // clang-format on
   }
 };
-}  // namespace
+}
 
 int main() {
-  sml::sm<states> sm;
-  sm.process_event(e1{});
-  sm.process_event(e2{});
-  sm.process_event(e3{});
-  assert(sm.is(sml::X));
+  using namespace sml;
 
-  return 0;
+  sm<hello_world> sm;
+  assert(sm.is(state<established>));
+
+  sm.process_event(release{});
+  assert(sm.is(state<fin_wait_1>));
+
+  sm.process_event(ack{});
+  assert(sm.is(state<fin_wait_2>));
+
+  sm.process_event(fin{});
+  assert(sm.is(state<timed_wait>));
+
+  sm.process_event(timeout{});
+  assert(sm.is(X));  // released
 }
